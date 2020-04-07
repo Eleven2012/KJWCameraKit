62f3e55b05489170cc2326ce20fd680811760b47
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:26:25 +0800

    init project

---
+++ KJWCameraKit/KJWCameraKit/KJWCameraKit.h
@@ -0,0 +1,19 @@
+//
+//  KJWCameraKit.h
+//  KJWCameraKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KJWCameraKit.
+FOUNDATION_EXPORT double KJWCameraKitVersionNumber;
+
+//! Project version string for KJWCameraKit.
+FOUNDATION_EXPORT const unsigned char KJWCameraKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KJWCameraKit/PublicHeader.h>
+
+
